8398c272b98e113387f87ac4481089868ebd7562
Livia Rosa <liviaalmeidarosa20@gmail.com>
2026-05-06 14:52:06 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/faculride/FaculRide.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.faculride;
+
+/**
+ *
+ * @author livia
+ */
+public class FaculRide {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
